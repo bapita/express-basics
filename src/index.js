7c05ed2c464a1003c1891b1fa278ae1468c5aa57
bapita <bapitaroy14@gmail.com>
2021-06-10 16:15:42 +0530

    expressJS code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 const path = require('path');
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+
 const staticPath = path.join(__dirname,"../public");
-const templatePath = path.join(__dirname,"../templates");
+const templatePath = path.join(__dirname,"../templates/views");
+const partialsPath = path.join(__dirname,"../templates/partials");
+//console.log(staticPath)
 
 // to set the view engine 
 app.set('view engine', "hbs"); // if pug/ EJS had been there then mention pug / EJS instead of hbs
 
 // We can rename views folder 
 app.set('views',templatePath);
+
+// Register Partials path
+hbs.registerPartials(partialsPath);
+
+// to serve a website using  built-in middleware express.static
+app.use(express.static(staticPath));
 
 // template engine route
 app.get("/", (req,res) => {
@@ -18,10 +28,26 @@ app.get("/", (req,res) => {
 })
 
 app.get("/about", (req,res) => {
-    res.render("about");
+    res.render('about', {
+        channelName : "Welcome Roy" // dynamic content to about.hbs
+    });
 })
-// to serve a website using  built-in middleware express.static
-app.use(express.static(staticPath));
+
+// Adding 404 custom error pages 
+
+app.get("/about/*", (req,res)=> {
+    res.render("404", {
+        errorcomment: "This Page of About us does not exist!"
+    })
+})
+
+app.get("*", (req,res) => {
+    res.render("404", {
+        errorcomment: "OOPS! Page couldn't be Found"
+    });
+})
+
+
 
 app.listen(8000, () => {
     console.log("listening 8000 port ..")
